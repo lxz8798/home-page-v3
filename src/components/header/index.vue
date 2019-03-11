92334d5fd800b7070497dc546b3162d4176d10ce
lixiaozhu <devilboy.l@qq.com>
2019-03-12 01:33:49 +0800

    显示图标

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -8,14 +8,14 @@
     </ul>
   </header>
   <header class="Header_wrap_mini" v-else>
-    <div class="Menu_List">
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-mulu1"></use>
-      </svg>
+    <svg class="icon" aria-hidden="true" @click="openToc()">
+      <use xlink:href="#icon-mulu1"></use>
+    </svg>
+    <div class="Menu_List">      
       <ul class="iconfont icon-mulu">
         <li v-for="(item,index) in menuList" :key="index" :class="{'activeHover':index == active}">
           <svg class="icon" aria-hidden="true">
-            <use :xlink:href="'#'+item.icon"></use>
+            <use :xlink:href="item.icon"></use>
           </svg>
           <!-- <i :class="item.icon">
             <router-link :to="item.link"></router-link>
@@ -86,60 +86,35 @@ header.Header-wrap {
 }
 header.Header_wrap_mini {
   z-index: 99;
-  div.Menu_List {
+  svg {
     position: fixed;
-    top: $spancin;
-    left: $spancin;
+    top: $boxRadius;
+    left: $boxRadius;
     width: 0.4rem;
     height: 0.4rem;
-    border-radius: 0.05rem;
-    transition: all 0.5s ease-in-out;
-    cursor: pointer;
-    svg {
-      width: 100%;
-      height: 100%;
-      fill: white;
-    }
+    fill: white;
   }
-  div.Menu_List:hover,
-  div.Menu_List:active {
+  div.Menu_List {
     position: fixed;
     top: 0;
     left: 0;
-    svg {
-      fill: $menuColor;
-      display: none;
-    }
+    width: $h5Width;
+    height: $childBaseHeight;
     ul {
       width: $h5Width;
-      height: $childBaseHeight;
-      border-radius: 0;
-      background: white;
+      height: inherit;
       display: flex;
       flex-direction: column;
       li {
-        width: inherit;
-        height: inherit;
         flex: 1;
-        box-sizing: border-box;
+        width: $h5Width;
+        height: 0.4rem;
+        border: 1px solid red;
 
-        i {
-          color: red;
-          width: inherit;
+        svg {
+          width: $h5Width;
           height: inherit;
-          display: inline-block;
         }
-      }
-    }
-  }
-}
-div.Header-wrap:hover {
-  background: rgba(255, 255, 255, 1);
-  transition: all 0.3s ease;
-  ul {
-    li {
-      a {
-        color: $menuColor;
       }
     }
   }
@@ -153,30 +128,31 @@ export default {
   props: ["active"],
   data() {
     return {
+      openTocList: false,
       scrollHidden: true,
       menuList: [
         {
           name: "LAZY-STUDIO.COM",
           link: "/",
-          icon: "iconfont icon-ERP_shouye",
+          icon: "#icon-ERP_shouye",
           key: 1
         },
         {
           name: "JAVASCRIPT/NODE",
           link: "jsview",
-          icon: "iconfont icon--java-script",
+          icon: "#icon--java-script",
           key: 2
         },
         {
           name: "CSS/ANIMATION",
           link: "cssview",
-          icon: "iconfont icon-css",
+          icon: "#icon-css",
           key: 3
         },
         {
           name: "DESIGN/PS/XD/AI/AE",
           link: "designview",
-          icon: "iconfont icon-caidanshejishi",
+          icon: "#icon-caidanshejishi",
           key: 4
         },
         {
@@ -188,25 +164,25 @@ export default {
         {
           name: "WORKS COLLECTION",
           link: "workview",
-          icon: "iconfont icon-3dcube",
+          icon: "#icon-3dcube",
           key: 6
         },
         {
           name: "UPDATE",
           link: "updateview",
-          icon: "iconfont icon-gengxin",
+          icon: "#icon-gengxin",
           key: 7
         },
         {
           name: "SIGN IN",
           link: "signinview",
-          icon: "iconfont icon-iddenglufanbai",
+          icon: "#icon-iddenglufanbai",
           key: 8
         },
         {
           name: "SIGN UP",
           link: "signupview",
-          icon: "iconfont icon-signout",
+          icon: "#icon-signout",
           key: 9
         }
       ]
@@ -222,6 +198,9 @@ export default {
   },
   methods: {
     ...mapMutations(["HOME_CURR_PUBLIC_WIDTH"]),
+    openToc() {
+      this.openTocList = true;
+    },
     /**
      * @Description: 有滑动的时候隐藏导航
      * @Author: 李啸竹
